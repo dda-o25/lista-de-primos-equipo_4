262e953da42165fe2f87978b57417a7a13ad47bc
chrisfb1901 <christian.flores@iteso.mx>
2025-10-08 09:33:10 -0600

    lista_primos

--- lista_primos.py
+++ lista_primos.py
@@ -1,15 +1,30 @@
 """
-Inserta el encabezado aquí y escribe tu código abajo
+
+ANGELICA ELIZABETH IBARRA DIAZ
+DIAZARTURO ARJONA HERMOSILLOA
+CHRISTIAN ALONSO FLORES BURGOS
+MARCO FABRIZIO AZPEITIA CASTELLANOS
+
+10 de octubre 2025
+
+Obtener la lista de todos los números primos menores a un número dado
+
 """
 
 # Declaraciones
-CONSTANTE = valor
 
 # Entradas
-entrada = input()
-
-# Proceso
-
-
+numero = int(input("Introduzca un número:"))
+es_primo = True
+cadena = []
+#Proceso 
+for i in range(2, numero):
+    es_primo = True
+    for x in range(2, i):
+        if i % x == 0:
+            es_primo = False
+    if es_primo:
+        cadena.append(i)
+             
 # Salidas
-print(salida)
+print(f"Los números primos menores o iguales a {numero} son: {cadena}")
